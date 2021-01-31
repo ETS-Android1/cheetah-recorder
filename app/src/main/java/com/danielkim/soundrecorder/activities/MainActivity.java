2c56aa90b550b361ecf9bd8fe58f4109a24b370a
Muhammad Saeed <msaeed06@uoguelph.ca>
2021-01-30 19:41:02 -0500

    New Project Intitial Commit Gradle version updated

---
+++ app/src/main/java/com/danielkim/soundrecorder/activities/MainActivity.java
@@ -0,0 +1,178 @@
+package com.danielkim.soundrecorder.activities;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.danielkim.soundrecorder.R;
+import com.danielkim.soundrecorder.fragments.FileViewerFragment;
+import com.danielkim.soundrecorder.fragments.LicensesFragment;
+import com.danielkim.soundrecorder.fragments.RecordFragment;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private PagerSlidingTabStrip tabs;
+    private ViewPager pager;
+
+    private int PERMISSION_CODE = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.RECORD_AUDIO) + ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+
+            //consider removing this due to constant after returning to MainActivity
+            Toast.makeText(MainActivity.this, "You have already granted this permission", Toast.LENGTH_SHORT).show();
+
+        } else {
+            requestPermission();
+        }
+
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new MyAdapter(getSupportFragmentManager()));
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabs.setViewPager(pager);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
+
+
+
+
+    private void requestPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.RECORD_AUDIO) || ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(MainActivity.this).
+                    setTitle("Permission Needed")
+                    .setMessage("Audio and Storage permission needed to use this app")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_CODE);
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        System.out.println("----------------------------------------------- " + grantResults[0]);
+        System.out.println("----------------------------------------------- " + grantResults[1]);
+
+        if(requestCode == PERMISSION_CODE) {
+
+            if(grantResults.length > 0 && (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED)){
+                Toast.makeText(MainActivity.this, "PERMISSION GRANTED", Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(MainActivity.this, "PERMISSION DENIED", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public class MyAdapter extends FragmentPagerAdapter {
+        private String[] titles = { getString(R.string.tab_title_record),
+                getString(R.string.tab_title_saved_recordings) };
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position){
+                case 0:{
+                    return RecordFragment.newInstance(position);
+                }
+                case 1:{
+                    return FileViewerFragment.newInstance(position);
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+    }
+
+    public MainActivity() {
+    }
+}
