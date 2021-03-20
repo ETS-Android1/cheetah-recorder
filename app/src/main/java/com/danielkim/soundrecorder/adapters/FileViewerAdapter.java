cbd3b9025ee1e0421269b74bc2ef1574e95875d0
bealj <bealj@uoguelph.ca>
2021-03-19 20:30:33 -0400

    Changed tag UI

--- app/src/main/java/com/danielkim/soundrecorder/adapters/FileViewerAdapter.java
+++ app/src/main/java/com/danielkim/soundrecorder/adapters/FileViewerAdapter.java
@@ -21,6 +21,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.Toast;
